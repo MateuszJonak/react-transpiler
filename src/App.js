5381b1244af7a5e8796ceaa8bf8f34d1f91c07b4
mateusz jonak <m.jonak@clearcode.cc>
2016-11-30 14:57:57 +0100

    Add babel + bootstrap and compile code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      output: '',
+      error: ''
+    };
+  }
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: window.Babel
+          .transform(code, {presets: ['es2015', 'react']})
+          .code,
+        error: ''
+      })
+    } catch(error) {
+      this.setState({error: error.message})
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        { this.state.error.length > 0 ? <header className="alert alert-danger">{this.state.error}</header> : null}
+        <div className="row">
+          <div className="col-xs-3">
+            <div className="form-group">
+              <div className="card card-block">
+                <h3 className="card-title">Code</h3>
+                <textarea
+                  className="form-control"
+                  onChange={this.update.bind(this)}
+                  defaultValue={this.state.input}/>
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-9">
+            <div className="card card-block">
+              <h3 className="card-title">Result</h3>
+              <pre className="card-text">
+                {this.state.output}
+              </pre>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
